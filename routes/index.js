7f68625a84c242431c5cf5116a067ae5988c0275
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-25 10:21:15 -0400

    set up

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Movie = require('../models/movie.js')
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Movie.find()
+        .then((movies) => {
+            res.render('index', { movies });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+});
+
+router.get('/movie/add', (req, res, next) => {
+    res.render('movie-add');
+});
+
+module.exports = router;
